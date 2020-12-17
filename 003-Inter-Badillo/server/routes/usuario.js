a3cc1ae79cebf668e42c8e910bf8018ed8aa9aef
PauBadMac16 <dianabadillomacias@gmail.com>
2020-12-16 21:56:15 -0600

    2da parte

--- 003-Inter-Badillo/server/routes/usuario.js
+++ 003-Inter-Badillo/server/routes/usuario.js
@@ -1,95 +1,90 @@
-const express = require('express');
-const _ = require('underscore');
-const usuario = require('../models/usuario');
-const app = express();
-
-app.get('/usuario', function (req, res) {
-  res.json({
-    ok: 200,
-    mensaje: "Usuario consultado"
-  })
-  })
-
-  let desde = req.query.desde || 0;
-  let hasta = req.query.hasta || 5;
-  
-  
-     Usuario.find({ estado: true })
-     .skip(Number(desde))
-     .limit(Number(hasta))
-     .exec((err, usuarios) => {
-        if (err) {
-          return res.status(400).json({
-            ok: false,
-            msg: 'Error al consultar',
-            err 
-          });
-        }
-        res.json({
-          ok: true,
-          msg: 'Muestra de usuarios exitosa',
-          conteo: usuarios.length,
-          usuarios 
-        });
-     });
-    });
-  
-    
-    app.post('/usuario', function(req,res){
-      let body = req.body;
-      let usr = new Usuario({
-        nombre: body.nombre,
-        primerApellido: body.primerapellido,
-        segundoApellido: body.segundoApellido,
-        edad: body.edad,
-        curp: body.curp,
-        telefono: body.telefono,
-        mail: body.email,
-
-
-      });
-
-      usr.save((err, usrDB) => {
-        if (err){
-          return res.status(400).json({
-            ok: false,
-            msg: 'Ocurrió un error',
-            err
-          });
-        }
-
-        res.json({
-          ok: true,
-          msg: 'Insercion exitosa',
-          usrDB
-        });
-      });
-  
-    app.put('/usuario/:id', function(req,res){
-        let id = req.params.id;
-        let body = _.pick(req.body, ['nombre', 'mail'])
-        Usuario.findByIdAndUpdate(id, body, 
-          { new: true, runValidators: true, context: 'query'},
-          (err, usrDB) => {
-          if (err) {
-            return res.status(400).json({
-              ok: false,
-              msg: 'Error al actualizar',
-              err 
+constexpress = require('express');
+const_ = require('underscore');
+constusuario = require('../models/usuario');
+constUsuario = require('../models/usuario');
+constapp = express();
+ 
+    app.get('/usuario', function (req, res) {
+    // Ordenar:
+    letdesde = req.query.desde || 0;
+    lethasta = req.query.hasta || 5;
+ 
+    Usuario.find({ estado:true })
+        .skip(Number(desde))
+        .limit(Number(hasta))
+        .exec((err, usuarios) => {
+    if (err) {
+        returnres.status(400).json({
+        ok:false,
+        msg:'Error al consultar',
+        err
             });
-          }
-          res.json({
-            ok: true,
-            msg: 'Actualizacion exitosa',
-            usuario: usrDB
-          });
-      });
+            }
+    // Respuesta exitosa:
+    res.json({
+    ok:true,
+    msg:'Muestreo exitoso',
+    conteo:usuarios.length,
+    usuarios
+            });
+        });
     });
-  
-  
+ 
+    app.post('/usuario', function(req,res){
+        letbody = req.body;
+        letusr = newUsuario({
+        nombre:body.nombre,
+        primerApellido:body.primerapellido,
+        segundoApellido:body.segundoApellido,
+        edad:body.edad,
+        curp:body.curp,
+        telefono:body.telefono,
+        mail:body.email,
+        activo:body.activo
+    });
+        
+    usr.save((err, usrDB) => {
+    // Si hubo algún error:
+    if (err){
+    returnres.status(400).json({
+        ok:false,
+        msg:'Algo fallo ',
+        err
+        });
+    }
+    res.json({
+    ok:true,
+    msg:'Insercion exitosa',
+    usrDB
+            });
+        });
+ 
+        });
+ 
+        app.put('/usuario/:id', function(req, res) {
+          let id = req.params.id;
+          let body = _.pick(req.body, ['nombre','primerApellido', 'segundoApellido', 'edad', 'curp', 'telefono', 'mail', 'activo');
+      
+          Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' },
+              (err, usrDB) => {
+                  if (err) {
+                      return res.status(400).json({
+                          ok: false,
+                          msg: 'Error al actualizar',
+                          err
+                      });
+                  }
+      
+                  res.json({
+                      ok: true,
+                      msg: 'Actualizacion exitosa',
+                      usuario: usrDB
+                  });
+              });
+      });
       app.delete('/usuario/:id', function(req, res){
         let id = req.params.id;
-  
+    
         Usuario.findByIdAndUpdate(id, {estado: false }, {new: true, runValidators: true, context: 'query'},
         (err, usrDB) =>{
           if (err){
@@ -106,9 +101,4 @@ app.get('/usuario', function (req, res) {
                 });
         });
       });
-  
-
-    });
-  
-    
     module.exports = app;
--- 003-Inter-Badillo/server/server.js
+++ 003-Inter-Badillo/server/server.js
@@ -1,8 +1,8 @@
-require ('./config/config.js');
+// require ('./config/config.js');
 const express = require ('express');    
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
-
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -10,39 +10,55 @@ app.use(bodyParser.json())
 
 
 app.get('/', function (req, res) {
-    res.send('Esta es mi practica intercuatrimestral');
+    res.send('Paseme!');
   });
-
+  app.use(require('./routes/usuario'));
+  
   app.get('/usuario', function (req, res){
     res.json({
         ok: 200, 
         mensaje:'Consulta exitosa'
     })
 })
-   app.post('/usuario', function (req, res){
-       let nombre = req.body.nombre;
-       let body = req.body;
-       res.json({
-           ok: 200, 
-           mensaje:'Insercion exitosa',
-          body:body 
-       })
-  })
-   //aqui se pone el delete
-
-
-
-   app.put('/usuario/:id/:nombre ', function (req, res){
+ 
+app.post('/usuario', function (req, res){
+    let nombre = req.body.nombre;
+    let body = req.body;
+    res.json({
+        ok: 200, 
+        mensaje:'Insercion exitosa',
+       body:body 
+    })
+})
+app.put('/usuario', function (req, res){
     let id = req.params.id
     let nombre = req.params.nombre;
     res.json({
         ok: 200,
-        mensaje: 'Usuario actualizado',
+        mensaje: 'Actualizacion exitosa',
         id: id,
         nombre: nombre
     })
 })
+app.delete ('/usuario/:id', function (req, res){ 
+    let id = req.params.id
+    
+    res.json({
+        ok: 200,
+        mensaje: 'Eliminacion exitosa',
+        id: id
+    })
+    })
+ mongoose.connect('mongodb://localhost:27017/003-Inter-Badillo', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+}, (err, res) => {
 
-  app.listen(process.env.PORT, () => {
-    console.log('El servidor está en línea en el puerto:  ', process.env.PORT);
-    });
+ if (err) throw err;
+ console.log('BDD ONLINE ');
+});
+  app.listen(3000, () => {
+      console.log('El servidor esta en la linea por el puerto 3000')
+  });
